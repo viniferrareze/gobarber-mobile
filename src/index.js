c5a148828b659dd0ea1fc924c1055ad0c496b597
carlos vinícius ferrareze <viniferrareze@gmail.com>
2020-03-09 21:35:48 -0300

    feat: configurando rotas privadas/publicas

--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { StatusBar } from 'react-native';
 
-import Routes from './routes';
+import App from './App';
+
 import { store, persistor } from './redux';
 
-export default function App() {
+export default function Index() {
    return (
       <Provider store={store}>
          <PersistGate persistor={persistor}>
             <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-            <Routes />
+            <App />
          </PersistGate>
       </Provider>
    );
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,29 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 
-export default createAppContainer(
-   createSwitchNavigator({
-      SignIn,
-      SignUp,
-   })
-);
+import Dashboard from './pages/Dashboard';
+
+export default (signedIn = false) =>
+   createAppContainer(
+      createSwitchNavigator(
+         {
+            // Cria um grupo de rotas para quando o usuário não estiver logado
+            Sign: createSwitchNavigator({
+               SignIn,
+               SignUp,
+            }),
+
+            // cria um grupo de rotas para quano o usuário estiver logado
+            App: createBottomTabNavigator({
+               // Cria a page como tipo tab bottom
+               Dashboard,
+            }),
+         },
+         {
+            initialRouteName: signedIn ? 'App' : 'Sign', // Se estiver logado pega o grupo de rotas App se nao Sign
+         }
+      )
+   );
