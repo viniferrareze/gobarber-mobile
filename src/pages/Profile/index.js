cfa0829b5d8668155f2508ad2a3b6ef15be1a9e9
carlos vinícius ferrareze <viniferrareze@gmail.com>
2020-03-09 21:59:15 -0300

    feat: Estilização das tabs e page

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
+import { View } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Background from '../../components/Background';
 
 // import { Container } from './styles';
 
-export default function Dashboard() {
+export default function Profile() {
    return <Background />;
 }
 
-// configuração das tabs e paginas
-Dashboard.navigationOptions = {
-   tabBarLabel: 'Agendamentos',
+Profile.navigationOptions = {
+   tabBarLabel: 'Perfil',
    tabBarIcon: ({ tintColor }) => (
       // Cria uma função que vem por parametro a cor do icone, e retorna um componente de icon
-      <Icon name="event" size={20} color={tintColor} />
+      <Icon name="person" size={20} color={tintColor} />
    ),
 };
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 
 import Dashboard from './pages/Dashboard';
+import Profile from './pages/Profile';
 
 export default (signedIn = false) =>
    createAppContainer(
@@ -17,10 +18,24 @@ export default (signedIn = false) =>
             }),
 
             // cria um grupo de rotas para quano o usuário estiver logado
-            App: createBottomTabNavigator({
-               // Cria a page como tipo tab bottom
-               Dashboard,
-            }),
+            App: createBottomTabNavigator(
+               {
+                  // Cria a page como tipo tab bottom
+                  Dashboard,
+                  Profile,
+               },
+               {
+                  // configurações das tabs
+                  tabBarOptions: {
+                     keyboardHidesTabBar: true, // quando o teclado abrir vai passar por cima do tabbar
+                     activeTintColor: '#fff',
+                     inactiveTintColor: 'rgba(255,255,255,0.6)',
+                     style: {
+                        backgroundColor: '#8d41a8',
+                     },
+                  },
+               }
+            ),
          },
          {
             initialRouteName: signedIn ? 'App' : 'Sign', // Se estiver logado pega o grupo de rotas App se nao Sign
