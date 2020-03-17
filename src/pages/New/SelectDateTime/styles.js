5eebbf972202710f1c2e2ab45a4c37489bef1bf8
carlos vinícius ferrareze <viniferrareze@gmail.com>
2020-03-16 23:30:57 -0300

    feat: Selecionando os horarios

--- src/pages/New/SelectDateTime/styles.js
+++ src/pages/New/SelectDateTime/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.SafeAreaView`
    flex: 1;
@@ -8,4 +9,27 @@ export const Piker = styled.View`
    background: #fff;
    padding: 15px 30px;
    margin-top: 30px;
+`;
+
+export const HourList = styled.FlatList.attrs({
+   numColumns: 2,
+   showVerticalScrollIndicator: 2,
+})`
+   padding: 0 20px;
+`;
+
+export const Hour = styled(RectButton)`
+   background: #fff;
+   border-radius: 4px;
+   padding: 20px;
+   flex: 1;
+   align-items: center;
+   margin: 0 10px 20px;
+   opacity: ${props => (props.enabled ? 1 : 0.6)};
+`;
+
+export const Title = styled.Text`
+   font-size: 14px;
+   font-weight: bold;
+   color: #333;
 `;
